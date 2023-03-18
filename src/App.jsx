12f57e8594e446e26d9a84060185da9c4085f1e4
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-17 23:02:24 -0300

    Componente barra de navegação

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import { Reset } from "./Reset"
+
+function App() {
+  return (
+    <>
+    <Header/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import Logo from '../assets/image//logo.png'
+import styled from "styled-components"
+
+export default function Navbar(){
+  return(
+    <BoxNav>
+      <img src={Logo} alt="Lush" />
+      <OptionNav>
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Planters</a>
+        <a href="">Contact</a>
+        <Btn>Call Us</Btn>
+      </OptionNav>
+    </BoxNav>
+  )
+}
+
+const BoxNav = styled.nav`
+  background-color: red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #ffffff;
+  padding: 37px 110px 0;
+`
+const OptionNav = styled.div`
+  display: flex;
+  gap: 11px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+`
+
+const Btn = styled.button`
+padding: 11px 50px;
+font-weight: 700;
+border: 1px solid #FFFFFF;
+border-radius: 3px;
+color: #FFFFFF;
+margin-left: 12px;
+`
