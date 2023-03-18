b6e172138839b125a8e1cf12715a7ed10c366977
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-18 17:56:44 -0300

    Componente de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,68 @@
-import Navbar from "./Navbar";
+import Navbar, { Btn } from './Navbar';
+import Banner from '../assets/image/banner.png'
+import IconPlay from '../assets/icone/play.svg'
+import styled from 'styled-components';
 
 export default function Header(){
   return(
-    <header>
+    <BoxHeader>
       <Navbar/>
-    </header>
+      <BoxContent>
+        <h1>Nature's Beauty Delivered to You</h1>
+        <p>Nature's beauty is just a click away with our online flower and plant shop. 
+          We offer a wide variety of flowers that will bring a touch of nature to your home!
+        </p>
+      </BoxContent>
+      <BoxButton>
+        <BtnGreen>Book Now</BtnGreen>
+        <BtnIcon>
+          <img src={IconPlay} alt="Click aqui" />
+        Watch Video
+        </BtnIcon>
+      </BoxButton>
+    </BoxHeader>
   )
 }
+
+const BoxHeader = styled.header`
+height: 758px;
+width: 100%;
+background-image: url(${Banner});
+background-size: cover;
+background-position: center;
+color: #ffffff;
+`
+
+const BoxContent = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+
+  h1{
+    Font-weight: 700;
+    font-size: 4rem;
+    margin: 156px 0 15px;
+  }
+
+  p{
+    width: 787px;
+    font-weight: 500;
+    font-size: 1.125rem;
+    text-align: center;
+    margin-bottom: 37px;
+  }
+`
+const BoxButton = styled.div`
+display: flex;
+gap: 13px;
+justify-content: center;
+`
+
+const BtnIcon = styled(Btn)`
+gap: 10px;
+`
+const BtnGreen = styled(Btn)`
+background: #285A43;
+border: #285A43;
+`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,26 +17,35 @@ export default function Navbar(){
 }
 
 const BoxNav = styled.nav`
-  background-color: red;
   display: flex;
   justify-content: space-between;
   align-items: center;
   color: #ffffff;
   padding: 37px 110px 0;
-`
+
+  `
 const OptionNav = styled.div`
   display: flex;
-  gap: 11px;
+  gap: 51px;
   font-weight: 500;
   display: flex;
   align-items: center;
-`
+  
+  a:hover{
+    text-decoration: underline;
+  }
+  
+  `
 
-const Btn = styled.button`
+export const Btn = styled.button`
 padding: 11px 50px;
 font-weight: 700;
 border: 1px solid #FFFFFF;
 border-radius: 3px;
 color: #FFFFFF;
 margin-left: 12px;
+background: transparent;
+display: flex;
+align-items: center;
+justify-content: center;
 `
